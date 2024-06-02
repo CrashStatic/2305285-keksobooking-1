f6590eb9c03a7cdd0b8e1ae31f8162cd1dcda98e
CrashStatic <thecrashstatic@gmail.com>
2024-06-02 15:01:46 +0300

    Меянет поведение слайдера

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import './form.js';
 import './filters.js';
-import './validate.js';
 import './slider.js';
+import './validate.js';
 import { generateAdvertisement } from './data.js';
 import { setFilterActive } from './filters.js';
 import { renderSimilarAdMarker } from './map.js';
